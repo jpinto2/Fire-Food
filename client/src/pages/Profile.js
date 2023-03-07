4266738269ba8b4a041c3b389dfe6f2a1223663c
Connor McGrath <connorjmcgrath93@gmail.com>
2023-03-06 16:39:48 -0800

    pulling from main

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  
+  return (
+    <div className="card bg-white card-rounded w-50">
+      Profile
+    </div>
+  );
+};
+
+export default Profile;
